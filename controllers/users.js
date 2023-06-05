32dc1a99052bc7cbc6ff19fbee9568e7e8e65a5c
Madina Bikteeva <112384268+BikteevaMadina@users.noreply.github.com>
2023-06-05 15:39:44 +0500

    7 fix users

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const userSchema = require('../models/user');
 const NotFoundError = require('../errors/NotFoundError');
 const BadRequestError = require('../errors/BadRequestError');
+const ConflictError = require('../errors/ConflictError');
 
 const {
   HTTP_STATUS_OK,
@@ -30,6 +32,65 @@ module.exports.getUserById = (request, response, next) => { // получаем 
 
       return next(err);
     });
+};
+
+module.exports.createUser = (request, response, next) => {
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = request.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      userSchema
+        .create({
+          name,
+          about,
+          avatar,
+          email,
+          password: hash,
+        })
+        .then(() => response.status(201)
+          .send(
+            {
+              data: {
+                name,
+                about,
+                avatar,
+                email,
+              },
+            },
+          ))
+        .catch((err) => {
+          if (err.code === 11000) {
+            return next(new ConflictError('The username with this email has already been registered'));
+          }
+          if (err.name === 'ValidationError') {
+            return next(new BadRequestError('Incorrect input'));
+          }
+          return next(err);
+        });
+    })
+    .catch(next);
+};
+
+module.exports.login = (request, response, next) => {
+  const {
+    email,
+    password,
+  } = request.body;
+
+  return userSchema
+    .findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'cat', {
+        expiresIn: '3d',
+      });
+      response.send({ token });
+    })
+    .catch(next);
 };
 
 module.exports.getUser = (request, response, next) => {
