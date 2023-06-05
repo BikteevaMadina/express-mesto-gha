919e4a515c3794ed704ab9bec3ffd42a7782cdfe
Madina Bikteeva <112384268+BikteevaMadina@users.noreply.github.com>
2023-06-05 15:41:53 +0500

    8 fix

--- controllers/auth.js
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const userSchema = require('../models/user');
-const BadRequestError = require('../errors/BadRequestError');
-const ConflictError = require('../errors/ConflictError');
-
